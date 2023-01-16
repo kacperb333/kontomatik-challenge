4e8e1e085bb63b6f467524364d9204e8291e1599
Kacper Bublik <kacper.bublik@kontomatik.com>
2023-01-16 13:28:01 +0100

    Changed pko service not to be visible outside lib module.

--- lib/src/main/java/com/kontomatik/pko/lib/PkoScraperFacadeFactory.java
+++ lib/src/main/java/com/kontomatik/pko/lib/PkoScraperFacadeFactory.java
@@ -5,7 +5,12 @@ import com.kontomatik.pko.lib.usecase.accounts.PkoAccountInfoUseCase;
 import com.kontomatik.pko.lib.usecase.login.PkoLoginUseCase;
 
 public class PkoScraperFacadeFactory {
-  public static PkoScraperFacade pkoScraperFacade(String baseUrl) {
+
+  public static PkoScraperFacade pkoScraperFacade() {
+    return pkoScraperFacade("https://www.ipko.pl/");
+  }
+
+  static PkoScraperFacade pkoScraperFacade(String baseUrl) {
     var pkoClient = PkoClientFactory.pkoClient(baseUrl);
     var pkoLoginUseCase = new PkoLoginUseCase(pkoClient);
     var pkoAccountInfoUseCase = new PkoAccountInfoUseCase(pkoClient);
