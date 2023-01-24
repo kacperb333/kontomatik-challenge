e2cc043cd054d1976eeb9a40c5ca11e88713f296
Kacper Bublik <kacper.bublik@kontomatik.com>
2023-01-24 11:40:09 +0100

    Refactored PkoConstants.

--- lib/src/main/java/com/kontomatik/lib/pko/domain/PkoConstants.java
+++ lib/src/main/java/com/kontomatik/lib/pko/domain/PkoConstants.java
@@ -1,5 +1,8 @@
 package com.kontomatik.lib.pko.domain;
 
-public interface PkoConstants {
-  String SESSION_HEADER_NAME = "x-session-id";
+public class PkoConstants {
+  public static String SESSION_HEADER_NAME = "x-session-id";
+
+  private PkoConstants() {
+  }
 }
